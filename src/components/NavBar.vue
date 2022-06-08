27399f414df6b2c4ef4b7231d4fd3b1ad3c80f7b
Kardast <dottorsf@gmail.com>
2022-06-08 16:55:40 +0200

    NavBar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,7 +3,16 @@
 
     <div class="container">
 
-      ciao
+      <div class="logo">
+
+        <img src="../assets/img/dc-logo.png" alt="dc-logo">
+      </div>
+      <ul class="navlist">
+        <li v-for="(link, index) in links" :key="index">
+          <a :class="link.current ? 'here' : ''" href="link.url">{{link.text}}</a>
+        </li>
+
+      </ul>
     </div>
   </div>
 </template>
@@ -11,6 +20,62 @@
 <script>
 export default {
   name: 'NavBar',
+  data(){
+    return{
+      links: [
+        {
+          text: "CHARACTERS",
+          url: "#",
+          current: false
+        },
+        {
+          text: "COMICS",
+          url: "#",
+          current: true
+        },
+        {
+          text: "MOVIES",
+          url: "#",
+          current: false
+        },
+        {
+          text: "TV",
+          url: "#",
+          current: false
+        },
+        {
+          text: "GAMES",
+          url: "#",
+          current: false
+        },
+        {
+          text: "COLLECATIBLES",
+          url: "#",
+          current: false
+        },
+        {
+          text: "VIDEOS",
+          url: "#",
+          current: false
+        },
+        {
+          text: "FANS",
+          url: "#",
+          current: false
+        },
+        {
+          text: "NEWS",
+          url: "#",
+          current: false
+        },
+        {
+          text: "SHOP",
+          url: "#",
+          current: false
+        },
+      ]
+    }
+  }
 }
 </script>
 
@@ -21,13 +86,52 @@ export default {
     height: 120px;
     display: flex;
     justify-content: center;
-    // debug
-    background-color: yellow;
-      .container{
-        width: 80%;
+
+    .container{
+      width: 80%;
+      height: 100%;
+      display: flex;
+
+      .logo{
         height: 100%;
-        // debug
-        background-color: red;
+        width: 40%;
+        display: flex;
+        align-items: center;
+        padding-left: 15px;
+          img{
+            width: 80px;
+          }
       }
+
+      .navlist{
+        padding: 0 30px;
+        width: 60%;
+        list-style: none;
+        display: flex;
+        align-items: center;
+        justify-content: end;
+
+        li{
+          margin: 0 5px;
+
+          a{
+            color: black;
+            text-decoration: none;
+            padding: 43px 5px;
+            font-size: 13px;
+            font-weight: bold;
+
+            &:hover{
+              color: violet;
+            }
+
+            &.here{
+              border-bottom: 8px solid rgb(0, 110, 255);
+              color: rgb(0, 110, 255);
+            }
+          }
+        }
+      }
+    }
   }
 </style>
